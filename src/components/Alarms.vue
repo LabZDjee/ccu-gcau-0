04e64a0796a75840cfb098ed1ed806c52d5e5d6a
Djee <ongola@free.fr>
2019-04-10 07:59:02 +0200

    alarms managed thru a v-for

--- src/components/Alarms.vue
+++ src/components/Alarms.vue
@@ -3,12 +3,10 @@
     <b-container fluid>
       <b-row>
         <b-col sm="6">
-          <alarm title="mains out-of-range" :data="alarmData[0]"></alarm>
-          <alarm title="high charge voltage" :data="alarmData[1]"></alarm>
+          <alarm v-for="alarmDef in leftAlarmDefs" :title="alarmDef.title" :data="alarmDef.data" :key="alarmDef.title"></alarm>
         </b-col>
         <b-col sm="6">
-          <alarm title="spare 1" :data="alarmData[2]"></alarm>
-          <alarm title="common alarm" :data="alarmData[3]"></alarm>
+          <alarm v-for="alarmDef in rightAlarmDefs" :title="alarmDef.title" :data="alarmDef.data" :key="alarmDef.title"></alarm>
         </b-col>
       </b-row>
     </b-container>
@@ -22,45 +20,59 @@ export default {
   name: "alarms",
   data() {
     return {
-      alarmData: [
-        // data: {enabledId, [relayNbId], [th1: {id, title, [unit]}, [th2: {id, title, [unit]}],
-        //       [delayId], [latch: {displayId, relayId}]}
+      // data: {enabledId, [relayNbId], [th1: {id, title, [unit]}, [th2: {id, title, [unit]}],
+      //       [delayId], [latch: {displayId, relayId}]}
+      leftAlarmDefs: [
         {
-          enabledId: "alarmMainsEnabled",
-          relayNbId: "alarmMainsRelay",
-          th1: { id: "alarmMainsMax", title: "max", unit: "V" },
-          th2: { id: "alarmMainsMin", title: "min", unit: "V" },
-          delayId: "alarmMainsDelay",
-          latch: {
-            displayId: "alarmMainsDisplayLatch",
-            relayId: "alarmMainsRelayLatch",
+          title: "mains out-of-range",
+          data: {
+            enabledId: "alarmMainsEnabled",
+            relayNbId: "alarmMainsRelay",
+            th1: { id: "alarmMainsMax", title: "max", unit: "V" },
+            th2: { id: "alarmMainsMin", title: "min", unit: "V" },
+            delayId: "alarmMainsDelay",
+            latch: {
+              displayId: "alarmMainsDisplayLatch",
+              relayId: "alarmMainsRelayLatch",
+            },
           },
         },
         {
-          enabledId: "alarmHCVEnabled",
-          relayNbId: "alarmHCVRelay",
-          th1: { id: "alarmHCVHighrate", title: "highrate", unit: "V" },
-          th2: { id: "alarmHCVFloating", title: "floating", unit: "V" },
-          delayId: "alarmHCVDelay",
-          latch: {
-            displayId: "alarmHCVDisplayLatch",
-            relayId: "alarmHCVRelayLatch",
+          title: "high charge voltage",
+          data: {
+            enabledId: "alarmHCVEnabled",
+            relayNbId: "alarmHCVRelay",
+            th1: { id: "alarmHCVHighrate", title: "highrate", unit: "V" },
+            th2: { id: "alarmHCVFloating", title: "floating", unit: "V" },
+            delayId: "alarmHCVDelay",
+            latch: {
+              displayId: "alarmHCVDisplayLatch",
+              relayId: "alarmHCVRelayLatch",
+            },
+          },
+        },
+      ],
+      rightAlarmDefs: [
+        {
+          title: "spare 1",
+          data: {
+            enabledId: "alarmSpare1Enabled",
+            relayNbId: "alarmSpare1Relay",
+            th1: { id: "alarmSpare1Text", title: "text" },
+            delayId: "alarmSpare1Delay",
+            latch: {
+              displayId: "alarmSpare1DisplayLatch",
+              relayId: "alarmSpare1RelayLatch",
+            },
           },
         },
         {
-          enabledId: "alarmSpare1Enabled",
-          relayNbId: "alarmSpare1Relay",
-          th1: { id: "alarmSpare1Text", title: "text" },
-          delayId: "alarmSpare1Delay",
-          latch: {
-            displayId: "alarmSpare1DisplayLatch",
-            relayId: "alarmSpare1RelayLatch",
-          },
-        },
-        {
-          enabledId: "alarmCommonEnbled",
-          latch: {
-            relayId: "alarmCommonRelayLatch",
+          title: "common alarm",
+          data: {
+            enabledId: "alarmCommonEnbled",
+            latch: {
+              relayId: "alarmCommonRelayLatch",
+            },
           },
         },
       ],
